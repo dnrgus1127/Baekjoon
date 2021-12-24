1aa5554a994dfcf9130300c3f8ec89f74d1c1730
dnrgus1127 <dnrgus1127@naver.com>
2021-12-24 22:24:26 +0900

    first

---
+++ 동적계획법+그래프/12852_1로만들기2/demo/src/main/java/com/example/App.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+       BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+       
+    }
+}
